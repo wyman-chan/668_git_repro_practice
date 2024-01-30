379f8a755492fc2f31c2d9a1f56fa8ed5899dff2
laurasisk <laurasisk42@gmail.com>
2024-01-29 21:52:14 -0800

    delete catnames2.py

--- cat_names2.py
+++
@@ -1,6 +0,0 @@
-print("My cat's names are Fiona and Ella. Fiona is doesn't like the roomba, so we named the roomba Farquad")
-
-
-#Below, write a print statement that will print "That's silly" when this code is run
-
-print("That's silly")
