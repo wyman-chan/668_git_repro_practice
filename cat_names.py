e5595cc9d36a3b7de4df093cb21885a1e8904582
Wyman Chan <wymanchan@Wymans-MacBook-Pro.local>
2025-01-24 11:19:42 -0800

    Added 'this is silly' to catnames.py

--- cat_names.py
+++ cat_names.py
@@ -2,3 +2,5 @@ print("My cat's names are Fiona and Ella. Fiona is doesn't like the roomba, so w
 
 
 #Below, write a print statement that will print "That's silly" when this code is run
+
+print("That's silly")
